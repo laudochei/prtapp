637c4074ca971b4fc2381f7fd58e743c79f3a5f9
Laud Ochei <User@DESKTOP-2IOGMA5.mshome.net>
2021-11-11 17:04:40 +0000

    Push to Github

--- src/main/java/prt/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
+++ src/main/java/prt/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
@@ -32,7 +32,7 @@ public class EmployeeServiceImpl implements EmployeeService{
         if(optional.isPresent()){
             employee = optional.get();
         }else{
-                throw new RuntimeException(" Employee not found for this id:: " + id);
+                throw new RuntimeException(" Employee was not found for this id:: " + id);
         }
         return employee;
     }
